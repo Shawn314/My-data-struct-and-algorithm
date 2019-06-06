b7d2d6141a3b5647e762eec1434f762c20e3dd83
fangxiao <fangxiao@pachiratech.com>
2019-06-06 09:54:00 +0800

    [ADD-binary_search]: binary search algorithm

---
+++ binary_search.py
@@ -0,0 +1,52 @@
+
+
+#二分查找算法递归和非递归实现
+#时间复杂度：最好(1), 最坏o(logn)
+
+def binary_search(alist, item):
+    #二分查找非递归方式
+    n = len(alist)
+    start = 0
+    end = n - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if alist[mid] == item:
+            return True
+        elif alist[mid] > item:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return False
+
+def binary_search_recursize(alist, item):
+    n = len(alist)
+    if 0 == n:
+        return False
+    mid = n // 2
+    if alist[mid] == item:
+        return True
+    elif alist[mid] > item:
+        binary_search_recursize(alist[:mid], item)
+    else:
+        binary_search_recursize(alist[mid:], item)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
